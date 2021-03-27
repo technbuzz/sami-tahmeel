2d9c4f3874f169fcc6a0c58bb3f0bb035f94f07f
Samiullah Khan <samiullah.khan.1989@gmail.com>
2021-03-27 12:07:49 +0400

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <Order />
   </div>
 </template>
 
 <script>
 
+import Order from './components/Order'
+
 export default {
   name: 'App',
   components: {
+    Order
   }
 }
 </script>
---
+++ src/components/Order.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>I am Order</h1>
+</template>
+
+<script>
+export default {
+  name: 'Order', 
+  props: {
+
+  }
+}
+</script>
