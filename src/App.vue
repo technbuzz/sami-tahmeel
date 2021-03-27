b29fc6d0a6580946f6881e0195ee296e782e88f4
Samiullah Khan <samiullah.khan.1989@gmail.com>
2021-03-27 12:20:19 +0400

    http request are set

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <Order />
+    <div v-for="(item, i) in pendingOrders" :key="i">
+      <Order />
+    </div>
   </div>
 </template>
 
@@ -13,6 +15,36 @@ export default {
   name: 'App',
   components: {
     Order
+  },
+  data () {
+    return {
+      pendingOrders: [],
+      assignedOrders: []
+    }
+  },
+  created () {
+    this.fetchPending()
+    this.fetchAssigned()
+  },
+  methods: {
+    async fetchPending (){
+      try {
+        const resp = await fetch('https://api.stagingtahmeelapp.com/technical_interview/orders/pending')
+        const data = await resp.json()
+        this.pendingOrders = data.pending_orders
+      } catch (error) {
+        console.log('error: ', error);
+      }
+    },
+    async fetchAssigned () {
+      try {
+        const resp = await fetch('https://api.stagingtahmeelapp.com/technical_interview/orders/assigned')
+        const data = await resp.json()
+        this.assignedOrders = data.assigned_orders
+      } catch (error) {
+        console.log('error: ', error);
+      }
+    }
   }
 }
 </script>
